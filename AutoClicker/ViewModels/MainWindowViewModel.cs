1bfe9f78de991d5097ff2d11f70977f1da466a4f
Denis Nikolenko <Demenolog0@yandex.ru>
2023-03-14 21:45:31 +0500

    Changing application title

--- AutoClicker/ViewModels/MainWindowViewModel.cs
+++ AutoClicker/ViewModels/MainWindowViewModel.cs
@@ -4,16 +4,6 @@ namespace AutoClicker.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        #region Title : string - title property
-
-        private string? _title = "Test name";
-
-        public string? Title
-        {
-            get => _title;
-            set => SetField(ref _title, value);
-        }
-
-        #endregion Title : string - title property
+        
     }
 }
