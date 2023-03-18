1ac28b912b24dd6e0e6c68774b1fea1ac85e4b7a
Denis Nikolenko <Demenolog0@yandex.ru>
2023-03-18 14:28:09 +0500

    Removed unused namespaces

--- AutoClicker/Views/Main/MainWindow.xaml.cs
+++ AutoClicker/Views/Main/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿namespace AutoClicker
+﻿using System.Windows.Controls;
+
+namespace AutoClicker
 {
     public partial class MainWindow
     {
         public MainWindow() => InitializeComponent();
+
     }
 }
