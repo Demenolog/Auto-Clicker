f58c9bff860655d09895217e3b24a838fb0a535f
Denis Nikolenko <Demenolog0@yandex.ru>
2023-04-19 21:49:35 +0500

    Added new class for counting total click times

---
+++ AutoClicker/Models/Other/TimeCounter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoClicker.Models.Other
+{
+    internal static class TimeCounter
+    {
+        public static int GetTotalTime(string hours, string minutes, string seconds, string milliseconds) =>
+            (int)new TimeSpan(0,
+                int.Parse(hours),
+                int.Parse(minutes),
+                int.Parse(seconds),
+                int.Parse(milliseconds)).TotalMilliseconds;
+    }
+}
