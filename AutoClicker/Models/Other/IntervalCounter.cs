1c551d8c3692aef422ea93508d5cb41f7d39cbef
Denis Nikolenko <Demenolog0@yandex.ru>
2023-04-26 21:17:14 +0500

    Some changes

--- AutoClicker/Models/Other/IntervalCounter.cs
+++ AutoClicker/Models/Other/IntervalCounter.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutoClicker.Models.Other
 {
     internal static class IntervalCounter
     {
-        public static int GetTotalIntervalTime(string hours, string minutes, string seconds, string milliseconds) =>
-            (int)new TimeSpan(0,
-                int.Parse(hours),
-                int.Parse(minutes),
-                int.Parse(seconds),
-                int.Parse(milliseconds)).TotalMilliseconds;
+        public static int GetTotalIntervalTime(string hours, string minutes, string seconds, string milliseconds)
+        {
+            var interval = (int)new TimeSpan(0, int.Parse(hours), int.Parse(minutes), int.Parse(seconds), int.Parse(milliseconds)).TotalMilliseconds;
+
+            return interval;
+
+            //return interval == 0 ? (int)(TimeSpan.FromSeconds(1).TotalMilliseconds) : interval;
+        }
     }
 }
