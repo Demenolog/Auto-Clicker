c397bb614162a0a974373d3ef80baec816e0cfd4
Denis Nikolenko <Demenolog0@yandex.ru>
2023-05-04 16:07:41 +0500

    Created service for work with hotkeys window

---
+++ AutoClicker/Services/HotKeysWindowService.cs
@@ -0,0 +1,38 @@
+﻿using AutoClicker.Views;
+
+namespace AutoClicker.Services
+{
+    internal static class HotKeysWindowService
+    {
+        private static HotKeyWindow s_hotKeyWindow = null;
+
+        public static bool IsCreated => s_hotKeyWindow != null;
+
+        public static HotKeyWindow HotKeyWindow
+        {
+            get => s_hotKeyWindow;
+            set => s_hotKeyWindow = value;
+        }
+
+        public static void Create()
+        {
+            if (HotKeyWindow != null) return;
+
+            HotKeyWindow = new HotKeyWindow();
+            HotKeyWindow.Closed += (o, args) => HotKeyWindow = null;
+
+            ChildWindowsService.Add(HotKeyWindow);
+        }
+
+        public static bool Show()
+        {
+            if (HotKeyWindow != null)
+            {
+                HotKeyWindow.Show();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
