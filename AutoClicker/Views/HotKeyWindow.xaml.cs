0a325ccc85ecd173f207b64d1e91cb43f0838f6e
Denis Nikolenko <Demenolog0@yandex.ru>
2023-05-04 15:43:09 +0500

    Created view and viewModel for HotKeys window

---
+++ AutoClicker/Views/HotKeyWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace AutoClicker.Views
+{
+    /// <summary>
+    /// Interaction logic for HotKeyWindow.xaml
+    /// </summary>
+    public partial class HotKeyWindow : Window
+    {
+        public HotKeyWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
