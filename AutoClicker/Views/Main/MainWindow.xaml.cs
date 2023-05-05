eb796330bdd9beae84b3afacfd4e5344224002f4
Denis Nikolenko <Demenolog0@yandex.ru>
2023-05-05 12:08:53 +0500

    Changed life cycle of main window

--- AutoClicker/Views/Main/MainWindow.xaml.cs
+++ AutoClicker/Views/Main/MainWindow.xaml.cs
@@ -1,8 +1,40 @@
-﻿namespace AutoClicker.Views.Main
+﻿using AutoClicker.Services;
+using System.Windows.Interop;
+using System;
+using System.Windows;
+using AutoClicker.Models.Hotkeys;
+
+namespace AutoClicker.Views.Main
 {
-    public partial class MainWindow
+    public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+
+        private HwndSource? _source;
+
+        #region Life cycle
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+            _source = HwndSource.FromHwnd(handle)!;
+            _source.AddHook(GlobalHotKey.HwndHook);
+
+            GlobalHotKey.RegisterHotKey(handle);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _source!.RemoveHook(GlobalHotKey.HwndHook);
+
+            ChildWindowsService.CloseAll();
+
+            base.OnClosed(e);
+        }
+
+        #endregion
 
     }
 }
