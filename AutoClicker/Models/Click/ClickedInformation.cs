2274573ffc987702c0c097df1bab9b3f4a019862
Denis Nikolenko <Demenolog0@yandex.ru>
2023-12-05 19:00:55 +0500

    [feat]: Created model with all information about clicks

---
+++ AutoClicker/Models/Click/ClickedInformation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoClicker.Models.Click.States;
+
+namespace AutoClicker.Models.Click
+{
+    internal class ClickedInformation
+    {
+        private Intervals _inrerval;
+        private Options _option;
+        private Repeats _repeat;
+        private Positions _position;
+
+        public ClickedInformation()
+        {
+            _inrerval = new Intervals();
+        }
+    }
+}
