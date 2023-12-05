78c6d1bab5ad3c729329d6797b3dfa53fcd8accc
Denis Nikolenko <Demenolog0@yandex.ru>
2023-12-05 19:00:06 +0500

    [feat]: Created click state models

---
+++ AutoClicker/Models/Click/States/Intervals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoClicker.ViewModels;
+
+namespace AutoClicker.Models.Click.States
+{
+    internal class Intervals
+    {
+
+    }
+}
