d188b8289bf4af6ccc987e4f32d0613532cae64f
Denis Nikolenko <Demenolog0@yandex.ru>
2023-12-21 18:08:30 +0500

    [feat]: Added additional fields for class

--- AutoClicker/Models/Clicks/States/Options.cs
+++ AutoClicker/Models/Clicks/States/Options.cs
@@ -9,17 +9,30 @@ namespace AutoClicker.Models.Clicks.States
     {
         private readonly string _button;
         private readonly string _buttonMode;
+        private readonly int _downMouseEventFlag;
+        private readonly int _upMouseEventFlag;
         private static readonly MainWindowViewModel MainWindow = ViewModelLocatorProvider.MainWindow;
 
         public Options()
         {
             _button = MainWindow.SelectedMouseButton;
             _buttonMode = MainWindow.SelectedMouseButtonMode;
+
+            (_downMouseEventFlag, _upMouseEventFlag) = Button switch
+            {
+                "Left" => ((int)MouseEventFlags.Leftdown, (int)MouseEventFlags.Leftup),
+                "Right" => ((int)MouseEventFlags.Rightdown, (int)MouseEventFlags.Rightup),
+                _ => throw new ArgumentException()
+            };
         }
 
         public string Button => _button;
 
         public string ButtonMode => _buttonMode;
+
+        public int DownMouseEventFlag => _downMouseEventFlag;
+
+        public int UpMouseEventFlag => _upMouseEventFlag;
 
         public int GetButtonMode()
         {
