d8bb5709766e83e31f6c79d0c49cae89da06c815
Denis Nikolenko <Demenolog0@yandex.ru>
2023-12-21 12:58:35 +0500

    [feat]: Created class for click options

--- AutoClicker/Models/Click/States/Options.cs
+++ AutoClicker/Models/Click/States/Options.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoClicker.Services.WindowHelper;
+using AutoClicker.ViewModels;
+using System;
+using static AutoClicker.Infrastructure.Constans.MouseClass.MouseClassConstans;
 
 namespace AutoClicker.Models.Click.States
 {
     internal class Options
     {
+        private string _mouseButton;
+        private string _mouseButtonMode;
+        private static readonly MainWindowViewModel MainWindow = ViewModelLocatorProvider.MainWindow;
+
+        public Options()
+        {
+            _mouseButton = MainWindow.SelectedMouseButton;
+            _mouseButtonMode = MainWindow.SelectedMouseButtonMode;
+        }
+
+        public int GetButtonMode()
+        {
+            return (int)Enum.Parse(typeof(ClickModes), _mouseButtonMode);
+        }
     }
 }
