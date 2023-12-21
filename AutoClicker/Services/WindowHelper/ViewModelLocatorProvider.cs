c9a44647bc17cc71127868a8825342dfd82e8f62
Denis Nikolenko <Demenolog0@yandex.ru>
2023-12-21 12:17:44 +0500

    [feat]: Created a class which create viewModel objects

---
+++ AutoClicker/Services/WindowHelper/ViewModelLocatorProvider.cs
@@ -0,0 +1,11 @@
+﻿using AutoClicker.ViewModels;
+
+namespace AutoClicker.Services.WindowHelper
+{
+    internal static class ViewModelLocatorProvider
+    {
+        public static readonly MainWindowViewModel MainWindow = new ViewModelLocator().MainWindowModel;
+
+        public static readonly HotKeyWindowViewModel HotKeyWindow = new ViewModelLocator().HotKeyWindowModel;
+    }
+}
