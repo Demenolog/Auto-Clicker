dc679570c66fc50a94555890e0364b894a4a8455
Denis Nikolenko <Demenolog0@yandex.ru>
2023-12-21 13:37:31 +0500

    [feat]: Created class for click position

---
+++ AutoClicker/Models/Click/States/Position.cs
@@ -0,0 +1,38 @@
+﻿using AutoClicker.Models.Mouse;
+using AutoClicker.Services.WindowHelper;
+using AutoClicker.ViewModels;
+using System.Drawing;
+
+namespace AutoClicker.Models.Click.States
+{
+    internal class Position
+    {
+        private Point _position;
+        private static readonly MainWindowViewModel MainWindow = ViewModelLocatorProvider.MainWindow;
+
+        public Position()
+        {
+            _position = GetPosition();
+        }
+
+        private Point GetPosition()
+        {
+            var isCurrentLocationSelected = MainWindow.IsCurrentLocationSelected;
+            Point position;
+
+            if (isCurrentLocationSelected)
+            {
+                position = MouseClicks.GetCurrentCursorPosition();
+            }
+            else
+            {
+                var x = int.Parse(MainWindow.XAxisTextBox);
+                var y = int.Parse(MainWindow.YAxisTextBox);
+
+                position = new Point(x, y);
+            }
+
+            return position;
+        }
+    }
+}
