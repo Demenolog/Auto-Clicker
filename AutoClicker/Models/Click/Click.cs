1160ee78f2afe2a44eca4110d935e74a83461869
Denis Nikolenko <Demenolog0@yandex.ru>
2023-12-21 13:51:13 +0500

    [feat]: Created ctor and properties for fields

---
+++ AutoClicker/Models/Click/Click.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoClicker.Models.Click.States;
+
+namespace AutoClicker.Models.Click
+{
+    internal class Click
+    {
+        private readonly Interval _interval;
+        private readonly Options _options;
+        private readonly Repeats _repeats;
+        private readonly Position _position;
+
+        public Click()
+        {
+            _interval = new Interval();
+            _options = new Options();
+            _repeats = new Repeats();
+            _position = new Position();
+        }
+
+        public Interval Interval => _interval;
+
+        public Options Options => _options;
+
+        public Repeats Repeats => _repeats;
+
+        public Position Position => _position;
+    }
+}
