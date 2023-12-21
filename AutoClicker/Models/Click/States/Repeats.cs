1189b2b80cb3a531ad9d429b43b5d5bc093465f6
Denis Nikolenko <Demenolog0@yandex.ru>
2023-12-21 13:24:38 +0500

    [feat]: Created class for click repeats

--- AutoClicker/Models/Click/States/Repeats.cs
+++ AutoClicker/Models/Click/States/Repeats.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoClicker.Services.WindowHelper;
+using AutoClicker.ViewModels;
 
 namespace AutoClicker.Models.Click.States
 {
     internal class Repeats
     {
+        private int _repeats;
+        private static readonly MainWindowViewModel MainWindow = ViewModelLocatorProvider.MainWindow;
+
+        public Repeats()
+        {
+            _repeats = GetRepeats();
+        }
+
+        private int GetRepeats()
+        {
+            var isEndless = MainWindow.IsRepeatUntilStoppedSelected;
+
+            return isEndless 
+                ? -1 
+                : int.Parse(MainWindow.RepeatTimesTextBox);
+        }
     }
 }
