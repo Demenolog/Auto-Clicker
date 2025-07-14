fa111240985fc8d65a494c2c3d7a22c0a833f25e
Demenolog <Demenolog0@yandex.ru>
2025-07-14 13:38:32 +0500

    [add]: Created a new class for auto key capturing

---
+++ AutoClicker/Begaviors/HotkeyCaptureBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace AutoClicker.Behaviors
+{
+    public class HotkeyCaptureBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.PreviewKeyDown += OnPreviewKeyDown;
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewKeyDown -= OnPreviewKeyDown;
+            base.OnDetaching();
+        }
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Build a name like "Ctrl" or "Alt+F5"
+            string keyName = e.Key == Key.System ? e.SystemKey.ToString() : e.Key.ToString();
+
+            if (Keyboard.Modifiers != ModifierKeys.None)
+                keyName = Keyboard.Modifiers + "+" + keyName;
+
+            var tb = (TextBox)sender;
+            tb.Text = keyName;
+            tb.CaretIndex = tb.Text.Length;
+
+            // Propagate to ViewModel
+            var binding = tb.GetBindingExpression(TextBox.TextProperty);
+            binding?.UpdateSource();
+
+            e.Handled = true;       // prevent default text input
+        }
+    }
+}
