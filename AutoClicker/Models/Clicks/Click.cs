9198dc8c99199d1310793cb6b94962ab2809ef79
Demenolog <Demenolog0@yandex.ru>
2025-07-17 13:10:25 +0500

    [add]: Created a new model

---
+++ AutoClicker/Models/Clicks/Click.cs
@@ -0,0 +1,29 @@
+﻿using AutoClicker.Models.Clicks.States;
+using Options = AutoClicker.Models.Clicks.States.Options;
+
+namespace AutoClicker.Models.Clicks
+{
+    internal class Click
+    {
+        private readonly Interval _interval;
+        private readonly Options _options;
+        private readonly Repeats _repeats;
+        private readonly Position _position;
+
+        public Click()
+        {
+            _interval = new Interval();
+            _options = new Options();
+            _repeats = new Repeats();
+            _position = new Position();
+        }
+
+        public Interval Interval => _interval;
+
+        public Options Options => _options;
+
+        public Repeats Repeats => _repeats;
+
+        public Position Position => _position;
+    }
+}
